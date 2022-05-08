bc2ebfddff5bfffcbbe2971c66ffb23cd17e1195
David Zhang <crispgm@gmail.com>
2022-05-08 16:47:25 +0800

    feat: support k

--- cmd/pelo/main.go
+++ cmd/pelo/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 5 {
+	if len(os.Args) < 5 {
 		pterm.Error.Println("Invalid params")
 	}
 
@@ -18,13 +18,17 @@ func main() {
 	t1p2Score := convertToFloat(os.Args[2])
 	t2p1Score := convertToFloat(os.Args[3])
 	t2p2Score := convertToFloat(os.Args[4])
+	k := 10.0
+	if len(os.Args) > 5 {
+		k = convertToFloat(os.Args[5])
+	}
 	rhw := elo.Rate{
 		T1P1Score: t1p1Score,
 		T1P2Score: t1p2Score,
 		T2P1Score: t2p1Score,
 		T2P2Score: t2p2Score,
 		HostWin:   true,
-		K:         10,
+		K:         k,
 	}
 	rhw.CalcEloRating()
 	raw := elo.Rate{
@@ -33,29 +37,29 @@ func main() {
 		T2P1Score: t2p1Score,
 		T2P2Score: t2p2Score,
 		HostWin:   false,
-		K:         10,
+		K:         k,
 	}
 	raw.CalcEloRating()
 
-	fmt.Println("Estimated Elo rating:")
+	fmt.Printf("Estimated Elo rating (k=%d):\n", int(k))
 
 	fmt.Println("- If host won:")
 	hostWonData := [][]string{
 		{"Team", "Player", "Cur Score", "Expect", "New Score", "Rating Change"},
-		{"1", "1", fmt.Sprintf("%.f", t1p1Score), fmt.Sprintf("%.2f%%", rhw.T1P1Exp*100), fmt.Sprintf("%.f", rhw.T1P1Score), pterm.Green("+", rhw.T1P1Score-t1p1Score)},
-		{"1", "2", fmt.Sprintf("%.f", t1p2Score), fmt.Sprintf("%.2f%%", rhw.T1P2Exp*100), fmt.Sprintf("%.f", rhw.T1P2Score), pterm.Green("+", rhw.T1P2Score-t1p2Score)},
-		{"2", "1", fmt.Sprintf("%.f", t2p1Score), fmt.Sprintf("%.2f%%", rhw.T2P1Exp*100), fmt.Sprintf("%.f", rhw.T2P1Score), pterm.Red(rhw.T2P1Score - t2p1Score)},
-		{"2", "2", fmt.Sprintf("%.f", t2p2Score), fmt.Sprintf("%.2f%%", rhw.T2P2Exp*100), fmt.Sprintf("%.f", rhw.T2P2Score), pterm.Red(rhw.T2P2Score - t2p2Score)},
+		{"A", "1", fmt.Sprintf("%.f", t1p1Score), fmt.Sprintf("%.2f%%", rhw.T1P1Exp*100), fmt.Sprintf("%.f", rhw.T1P1Score), pterm.Green("+", rhw.T1P1Score-t1p1Score)},
+		{"A", "2", fmt.Sprintf("%.f", t1p2Score), fmt.Sprintf("%.2f%%", rhw.T1P2Exp*100), fmt.Sprintf("%.f", rhw.T1P2Score), pterm.Green("+", rhw.T1P2Score-t1p2Score)},
+		{"B", "1", fmt.Sprintf("%.f", t2p1Score), fmt.Sprintf("%.2f%%", rhw.T2P1Exp*100), fmt.Sprintf("%.f", rhw.T2P1Score), pterm.Red(rhw.T2P1Score - t2p1Score)},
+		{"B", "2", fmt.Sprintf("%.f", t2p2Score), fmt.Sprintf("%.2f%%", rhw.T2P2Exp*100), fmt.Sprintf("%.f", rhw.T2P2Score), pterm.Red(rhw.T2P2Score - t2p2Score)},
 	}
 	pterm.DefaultTable.WithHasHeader().WithData(hostWonData).WithBoxed(true).Render()
 
 	fmt.Println("- If away won:")
 	awayWonData := [][]string{
 		{"Team", "Player", "Cur Score", "Expect", "New Score", "Rating Change"},
-		{"1", "1", fmt.Sprintf("%.f", t1p1Score), fmt.Sprintf("%.2f%%", raw.T1P1Exp*100), fmt.Sprintf("%.f", raw.T1P1Score), pterm.Red(raw.T1P1Score - t1p1Score)},
-		{"1", "2", fmt.Sprintf("%.f", t1p2Score), fmt.Sprintf("%.2f%%", raw.T1P2Exp*100), fmt.Sprintf("%.f", raw.T1P2Score), pterm.Red(raw.T1P2Score - t1p2Score)},
-		{"2", "1", fmt.Sprintf("%.f", t2p1Score), fmt.Sprintf("%.2f%%", raw.T2P1Exp*100), fmt.Sprintf("%.f", raw.T2P1Score), pterm.Green("+", raw.T2P1Score-t2p1Score)},
-		{"2", "2", fmt.Sprintf("%.f", t2p2Score), fmt.Sprintf("%.2f%%", raw.T2P2Exp*100), fmt.Sprintf("%.f", raw.T2P2Score), pterm.Green("+", raw.T2P2Score-t2p2Score)},
+		{"A", "1", fmt.Sprintf("%.f", t1p1Score), fmt.Sprintf("%.2f%%", raw.T1P1Exp*100), fmt.Sprintf("%.f", raw.T1P1Score), pterm.Red(raw.T1P1Score - t1p1Score)},
+		{"A", "2", fmt.Sprintf("%.f", t1p2Score), fmt.Sprintf("%.2f%%", raw.T1P2Exp*100), fmt.Sprintf("%.f", raw.T1P2Score), pterm.Red(raw.T1P2Score - t1p2Score)},
+		{"B", "1", fmt.Sprintf("%.f", t2p1Score), fmt.Sprintf("%.2f%%", raw.T2P1Exp*100), fmt.Sprintf("%.f", raw.T2P1Score), pterm.Green("+", raw.T2P1Score-t2p1Score)},
+		{"B", "2", fmt.Sprintf("%.f", t2p2Score), fmt.Sprintf("%.2f%%", raw.T2P2Exp*100), fmt.Sprintf("%.f", raw.T2P2Score), pterm.Green("+", raw.T2P2Score-t2p2Score)},
 	}
 	pterm.DefaultTable.WithHasHeader().WithData(awayWonData).WithBoxed(true).Render()
 }
