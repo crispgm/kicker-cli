b63a04d75cb34ad7bae60e3e2d6dbbe5902c258d
David Zhang <crispgm@gmail.com>
2023-09-06 14:23:27 +0800

    feat: global no color

--- cmd/kicker-cli/cmd/root.go
+++ cmd/kicker-cli/cmd/root.go
@@ -2,15 +2,23 @@
 package cmd
 
 import (
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 
 	"github.com/crispgm/kicker-cli/internal/app"
 )
 
+var globalNoColor bool
+
 var rootCmd = &cobra.Command{
 	Use:     "kicker-cli",
 	Long:    "A Foosball data aggregator, analyzers, and manager based on Kickertool.",
 	Version: app.Version,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if globalNoColor {
+			pterm.DisableColor()
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Usage()
 	},
@@ -18,6 +26,7 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	cobra.OnInitialize()
+	rootCmd.PersistentFlags().BoolVarP(&globalNoColor, "no-colors", "", false, "show no colors")
 }
 
 // Execute .
