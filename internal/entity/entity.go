9b8377028a049dea8fb6d428265eb36218b9cd5b
David Zhang <crispgm@gmail.com>
2023-09-11 14:58:55 +0800

    chore: rename operators

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -3,9 +3,9 @@ package entity
 
 // Supported modes
 const (
-	ModeDoublePlayerRanks  = "double_player_ranks"
-	ModeDoubleTeamRanks    = "double_team_ranks"
-	ModeDoubleTeamRivals   = "double_team_rivals"
-	ModeSinglePlayerRanks  = "single_player_ranks"
-	ModeSinglePlayerRivals = "single_player_rivals"
+	ModeDoublePlayerRank  = "double_player_rank"
+	ModeDoubleTeamRank    = "double_team_rank"
+	ModeDoubleTeamRival   = "double_team_rival"
+	ModeSinglePlayerRank  = "single_player_rank"
+	ModeSinglePlayerRival = "single_player_rival"
 )
