6c0d070006f25d8562f7852e322bcd75176acc85
David Zhang <crispgm@gmail.com>
2023-12-11 17:35:24 +0800

    feat(player): show aliases

--- cmd/kicker-cli/cmd/player.go
+++ cmd/kicker-cli/cmd/player.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 
@@ -32,7 +34,7 @@ func listPlayerCommand(cmd *cobra.Command, args []string) {
 	instance := initInstanceAndLoadConf()
 	// load tournaments
 	var table [][]string
-	header := []string{"ID", "Name", "ITSF_ID", "ATSA_ID"}
+	header := []string{"ID", "Name", "ITSF_ID", "ATSA_ID", "A.K.A"}
 	if !globalNoHeaders {
 		table = append(table, header)
 	}
@@ -46,8 +48,9 @@ func listPlayerCommand(cmd *cobra.Command, args []string) {
 		table = append(table, []string{
 			p.ID,
 			p.Name,
-			p.ITSFID,
-			p.ATSAID,
+			dashIfEmpty(p.ITSFID),
+			dashIfEmpty(p.ATSAID),
+			dashIfEmpty(strings.Join(p.Aliases, ", ")),
 		})
 	}
 	_ = pterm.DefaultTable.WithHasHeader(!globalNoHeaders).WithData(table).WithBoxed(!globalNoBoxes).Render()
