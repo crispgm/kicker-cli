474b29dcbf6770737d1676d04a5ceda10bbb8ad9
Luna <chloe@transgender.army>
2024-02-24 22:31:31 +0100

    removed unnecisary description

--- flake.nix
+++ flake.nix
@@ -1,25 +1,33 @@
 {
-  description = "glsl learning project";
   inputs = {
     #flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, nvim-conf,nixvim}:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    
-    
-  in{
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.go
-        pkgs.gopls
-      ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
 
-      shellHook = ''
-        export inNixShell=1
-        echo "hello World" ...
-      '';
+  } @ inputs:
+  flake-parts.lib.mkFlake {inherit inputs;} {
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    
+    perSystem = {
+      pkgs,
+      system,
+      ...
+    }:
+    {
+      packages = {
+        default = pkgs.callPackage ./pkg.nix {};
+                     
+      };
     };
-  };  
+  };
 }
