c2f8f7c4c7480e2b97574760e0fb9c69bc07b62f
Luna <chloe@transgender.army>
2024-02-26 14:19:03 +0100

    forgot to actually commit the important stuff???

--- pkg.nix
+++ pkg.nix
@@ -1,20 +1,28 @@
 {
 buildGoModule,
-fetchFromGithub,
+fetchFromGitHub,
+stdenv,
+lib,
 ...
 }:
 
 
 
-buildGoModule {
+stdenv.mkDerivation buildGoModule rec {
     pname = "kicker-cli";
-    version = "1.0.0"; #there are no versions?
-    
-    src = fetchFromGithub {
+    version = "1.0.0"; #there are no versions?    
+    src = fetchFromGitHub {
       owner = "crispgm";
-      repo = "kicker-cli";
-      #rev = latest 
-      sha256 = "sha256-YsR2KU5Np6xQHkjM8KAoDp/XZ/9DkwBlMbu2IX5OQlk=";
+      repo = "${pname}";
+      rev = "18fb2d566fb62f68c90d19468bb5dc545ea47da7"; 
+      sha256 = "0xwmpnznys6v7sl7rvnqhjdy78l17hw3bqgi8m12iyw7m1plj6nv";
+    };
+    vendorHash =  "sha256-sK+blqs5DDcF2Am6GNpkZhQ/AvoDqpxrWZa4rb3v5iE=";
+    meta = with lib; {
+      description = "Simple command-line snippet manager, written in Go";
+      homepage = "https://github.com/knqyf263/pet";
+      license = licenses.mit;
+      maintainers = with maintainers; [ kalbasit ];
     };
 }
 
