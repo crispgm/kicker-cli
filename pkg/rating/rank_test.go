8cbeb1df6acefb5c1b9e674611d556c6f21601f3
David Zhang <crispgm@gmail.com>
2025-03-31 14:12:23 +0800

    fix(rank): update test with new data

--- pkg/rating/rank_test.go
+++ pkg/rating/rank_test.go
@@ -18,17 +18,17 @@ func TestRank(t *testing.T) {
 	assert.Equal(t, 50.0, r.Calculate(f))
 
 	f.Place = 2
-	assert.Equal(t, 30.0, r.Calculate(f))
+	assert.Equal(t, 35.0, r.Calculate(f))
 	f.Place = 3
-	assert.Equal(t, 20.0, r.Calculate(f))
+	assert.Equal(t, 25.0, r.Calculate(f))
 	f.Place = 4
-	assert.Equal(t, 17.0, r.Calculate(f))
+	assert.Equal(t, 20.0, r.Calculate(f))
 	f.Place = 5
 	assert.Equal(t, 15.0, r.Calculate(f))
 	f.Place = 6
 	assert.Equal(t, 15.0, r.Calculate(f))
 	f.Place = 7
-	assert.Equal(t, 12.0, r.Calculate(f))
-	f.Place = 9
 	assert.Equal(t, 9.0, r.Calculate(f))
+	f.Place = 9
+	assert.Equal(t, 8.0, r.Calculate(f))
 }
